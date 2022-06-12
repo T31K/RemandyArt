1b923260c326114db7132d108a3b0b715d53c589
T31K <t31kmunwong@gmail.com>
2022-06-12 14:34:32 +0800

    Fix meta and redirect links

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,22 +30,10 @@
 
   const meta = 
   `
-    <title>Remandy Art | specializing in watercolour loose floral paintings, workshops and personalised gifts, based in Kuala Lumpur Malaysia.</title>
-    <meta name="title" content="Remandy Art | specializing in watercolour loose floral paintings, workshops and personalised gifts, based in Kuala Lumpur Malaysia.">
-    <meta name="description" content="Remandy.art is a small business specializing in watercolour loose floral paintings, classes, lessons, workshops and personalised gifts, based in Kuala Lumpur, Malaysia.">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://remandy.art/">
-    <meta property="og:title" content="Remandy Art | specializing in watercolour loose floral paintings, workshops and personalised gifts, based in Kuala Lumpur Malaysia.">
-    <meta property="og:description" content="Remandy.art is a small business specializing in watercolour loose floral paintings, classes, lessons, workshops and personalised gifts, based in Kuala Lumpur, Malaysia.">
-    <meta property="og:image" content="https://raw.githubusercontent.com/T31K/RemandyArt/master/assets/img/meta/meta_full.png">
-    <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://remandy.art/">
-    <meta property="twitter:title" content="Remandy Art | specializing in watercolour loose floral paintings, workshops and personalised gifts, based in Kuala Lumpur Malaysia.">
-    <meta property="twitter:description" content="Remandy.art is a small business specializing in watercolour loose floral paintings, classes, lessons, workshops and personalised gifts, based in Kuala Lumpur, Malaysia.">
-    <meta property="twitter:image" content="https://raw.githubusercontent.com/T31K/RemandyArt/master/assets/img/meta/meta_full.png">
+
   `
   
-  $('head').append(meta)
+  $('head').append
 
   /*---------- 02. Preloader ----------*/
   if ($('.preloader').length > 0) {
@@ -478,3 +466,5 @@
 })(jQuery);
 
 // lazy loads elements with default selector as '.lozad'
+const observer = lozad(); 
+observer.observe();
